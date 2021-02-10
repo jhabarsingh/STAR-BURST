86fb72ec3643d3251c333a4245008f2741c67bb4
jhabarsingh <43932986+jhabarsingh@users.noreply.github.com>
2021-02-11 03:51:25 +0530

    Refactored index.js code

--- src/store/index.js
+++ src/store/index.js
@@ -5,137 +5,139 @@ Vue.use(Vuex)
 import router from "../router/index"
 export default new Vuex.Store({
   state: {		
-	  		data:[		
-	  			],
-	  			events:[
-	  			],
-	  			toogle: true
-
+	  data:[],
+	  events:[],
+	  toogle: true
  },
-
- getters:{
- 	allData: function(state){
- 			return state.data
+ getters: {
+ 	allData: function(state) {
+ 		// Returns the whole data;	
+    return state.data;
  	},
- 	allEvents: function(state){
- 			return state.events
+ 	allEvents: function(state) {
+    // Returns the whole events;
+ 		return state.events;
  	}
  },
  mutations: {
-
- 	check: function(state){
- 		state.toogle = !state.toogle
+ 	check: function(state) {
+    // Toogle the toogle data;
+ 		state.toogle = !state.toogle;
  	}
-
+ },
+ actions: {
+  checkme: function({commit}) {
+    // Action to commit check Mutation;
+  	commit("check");
   },
-  actions: {
-  	checkme: function({commit}){
-  		commit("check")
-  	},
-  	autoLogout({commit}){
-  		setTimeout(()=>{
-  			router.push("/login")
-  		}, 3600000)
-  	},
-  	updateEvents({commit, state}, data){
-  			axios.put("https://stardust-4e9ca.firebaseio.com/events/"+ data.id +".json/?auth="+localStorage.getItem("idToken"),
-  		{
-  			title: data.title,
-  			about: data.about,
-  			url: data.url
-  		})
-  		.then(request=>{
-
-  		},err=>{
-  			console.log(err)
-  		})
-  	},
-  	deleteEvents({commit, state}, id){
-  		axios.delete("https://stardust-4e9ca.firebaseio.com/events/"+ id +".json/?auth="+localStorage.getItem("idToken"))
-  		.then(request=>{
-
-  		},err=>{
-  			console.log(err)
-  		})	
-  	},
-  	postEvents({commit, state}, data){
+   
+  autoLogout({commit}){
+    // Action to logout;
+  	setTimeout(()=>{
+  		router.push("/login");
+  	}, 3600000)
+  },
+  updateEvents({commit, state}, data){
+    // Action to update the Events;
+  	axios.put("https://stardust-4e9ca.firebaseio.com/events/"+ data.id +".json/?auth="+localStorage.getItem("idToken"),
+  	{
+  		title: data.title,
+  		about: data.about,
+  		url: data.url
+  	})
+    .then(request => {},
+      err => {
+  			console.log(err);
+  	})
+  },
+  deleteEvents({commit, state}, id){
+    // Action to delete the events;
+    axios.delete("https://stardust-4e9ca.firebaseio.com/events/"+ id +".json/?auth="+localStorage.getItem("idToken"))
+    .then(request => {},
+      err => {
+       console.log(err);
+    })	
+  },
+  postEvents({commit, state}, data) {
+    // action to add a event in the database;
 		axios.post("https://stardust-4e9ca.firebaseio.com/events.json/?auth="+localStorage.getItem("idToken"), data)
-		.then(request=>{
-			console.log(request)
-		},err=>{
-			console.log(err)
+		.then(request => {
+			console.log(request);
+		},err => {
+			console.log(err);
 		})
-  	},
-  	getEvents({commit, state}){
-		axios.get("https://stardust-4e9ca.firebaseio.com/events.json/")
-		.then(request=>{
-			let li = []
-			const events = request.data
-			for(let a in events){
-				const data = {
-					id: a,
-					title: events[a].title,
-					about: events[a].about,
-					url: events[a].url
-				}
-				console.log(data)
-				li.push(data)
-			}
-			state.events = li
-		},err=>{
-			console.log(err)
-		})
-  	},
-
-  	//
-  	updateTeam({commit, state}, data){
-  			axios.put("https://stardust-4e9ca.firebaseio.com/teams/"+ data.id +".json/?auth="+localStorage.getItem("idToken"),
-  		{
-  			name: data.name,
+  },
+  getEvents({commit, state}) {
+    // action to get all the events from the database;
+    axios.get("https://stardust-4e9ca.firebaseio.com/events.json/")
+    .then(request => {
+      let li = []
+      const events = request.data
+      for(let a in events) {
+        const data = {
+          id: a,
+          title: events[a].title,
+          about: events[a].about,
+          url: events[a].url
+        }
+        console.log(data)
+        li.push(data)
+      }
+      state.events = li
+    },err =>  {
+      console.log(err)
+    })
+  },
+  updateTeam({commit, state}, data){
+  	// Action to update the team;
+    axios.put("https://stardust-4e9ca.firebaseio.com/teams/"+ data.id +".json/?auth="+localStorage.getItem("idToken"),
+  	{
+  		name: data.name,
 			designation: data.designation,
 			url: data.url
-  		})
-  		.then(request=>{
-
-  		},err=>{
-  			console.log(err)
-  		})
-  	},
-  	deleteTeam({commit, state}, id){
-  		axios.delete("https://stardust-4e9ca.firebaseio.com/teams/"+ id +".json/?auth="+localStorage.getItem("idToken"))
-  		.then(request=>{
-
-  		},err=>{
-  			console.log(err)
-  		})	
-  	},
-  	postTeam({commit, state}, data){
-		axios.post("https://stardust-4e9ca.firebaseio.com/teams.json/?auth="+localStorage.getItem("idToken"), data)
-		.then(request=>{
-			console.log(request)
-		},err=>{
-			console.log(err)
+  	})
+  	.then(request=>{},
+       err => {
+  			 console.log(err)
+  	})
+  },
+  deleteTeam({commit, state}, id){
+  	// Action to delete the team;
+    axios.delete("https://stardust-4e9ca.firebaseio.com/teams/"+ id +".json/?auth="+localStorage.getItem("idToken"))
+  	.then(request => {},
+       err => {
+  			console.log(err);
+  	})	
+  },
+  postTeam({commit, state}, data){
+		// Action to create a team in the database;
+    axios.post("https://stardust-4e9ca.firebaseio.com/teams.json/?auth="+localStorage.getItem("idToken"), data)
+		.then(request => {
+			console.log(request);
+		},err => {
+			console.log(err);
 		})
-  	},
-  	getTeam({commit, state}){
-		axios.get("https://stardust-4e9ca.firebaseio.com/teams.json/")
-		.then(request=>{
-			let li = []
+  },
+  getTeam({commit, state}){
+		// Action to get all the team members details;
+    axios.get("https://stardust-4e9ca.firebaseio.com/teams.json/")
+		.then(request => {
+			let li = [];
 			const teams = request.data
-			for(let a in teams){
+			for(let a in teams) {
 				const data = {
 					id: a,
 					name: teams[a].name,
 					designation: teams[a].designation,
 					url: teams[a].url
 				}
-				console.log(data)
+				console.log(data);
 				li.push(data)
 			}
-			state.data = li
-		},err=>{
-			console.log(err)
+			state.data = li;
+		},err => {
+			console.log(err);
 		})
-  	},
-  }
+  },
+ }
 })
