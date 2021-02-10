173d9481bcff178e5447b0486482df1fdd3750e4
jhabarsingh <43932986+jhabarsingh@users.noreply.github.com>
2021-02-11 03:37:09 +0530

    intended App.vue Code

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 export default {
-  components:{
+  components: {
     Header,
     Footer
   },
-  created(){
+  created() {
     this.$store.dispatch("getEvents")
     this.$store.dispatch("getTeam")
   }
@@ -24,40 +24,42 @@ export default {
 </script>
 
 <style scoped>
-  #apps{
+  #apps {
     background: #17141D;
   }
 
-  .fade-enter{
+  .fade-enter {
     opacity: 0;
   }
 
-  .fade-enter-active{
+  .fade-enter-active {
       transition: 500ms linear;
       animation: up 1s ease-in;
   }
 
-  .fade-leave{
+  .fade-leave {
   }
 
-  .fade-leave-active{
-      opacity :0;
+  .fade-leave-active {
+      opacity : 0;
       transition: 500ms linear;
       animation: down 1s ease-out;
   }
-  @keyframes up{
-    from{
+  @keyframes up {
+    from {
       transform: translateY(40px);
-    }to{
+    }
+    to {
       transform: translateY(0px);
     }
   }
 
   
-  @keyframes down{
-    from{
+  @keyframes down {
+    from {
       transform: translateY(0px);
-    }to{
+    }
+    to {
       transform: translateY(40px);
     }
   }
