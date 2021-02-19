c97c391a10d015ed0d600b2013e9dc3bcbbe0299
jhabarsingh <43932986+jhabarsingh@users.noreply.github.com>
2021-02-19 15:28:02 +0530

    Updated Login.vue

--- src/components/content/Login.vue
+++ src/components/content/Login.vue
@@ -5,42 +5,34 @@
 				<form @submit.prevent='authenticate()'>
 				  <div class="form-group">
 				    <label for="exampleInputEmail1">Email</label>
-				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-						v-model="email"
-				    >
+				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email" />
 				  </div>
 				  <div class="form-group">
 				    <label for="exampleInputPassword1">Password</label>
-				    <input type="password" class="form-control" id="exampleInputPassword1"
-						v-model="password"
-				    >
+				    <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" >
 				  </div>
-				  <button type="submit" class="btn btn-primary" 
-						@click="$store.commit('check')"
-				  >Submit</button>
+				  <button type="submit" class="btn btn-primary" @click="$store.commit('check')" >Submit</button>
 				</form>
 			</div>
 	</div>
 	</div>
-	
 </template>
 
 
 <script>
 import axios from "axios"
-
-export default{
-	mounted(){
-		TweenMax.from(".jumbos", 1, {opacity:0, scale:0, delay:.5})
+export default {
+	mounted() {
+		TweenMax.from(".jumbos", 1, {opacity:0, scale:0, delay:.5});
 	},
-	data(){
-		return{
+	data() {
+		return {
 			email: "",
 			password: ""
 		}
 	},
-	methods:{
-		authenticate:function(){
+	methods: {
+		authenticate:function() {
 			axios.post("https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAnE-P1m4W9yudurKXgrOCUUA6Whr5bZMM",
 			{
 				email: this.email,
@@ -48,12 +40,12 @@ export default{
 				returnSecureToken: true
 			})
 			.then(rel=>{
-				localStorage.setItem("idToken", rel.data.idToken)
-				localStorage.setItem("expiresIn", new Date(new Date().getTime() + rel.data.expiresIn*1000))
-				this.$router.push("/loginhome")
-				this.$store.dispatch("autoLogout")
-			},err=>{
-				console.log(err)
+				localStorage.setItem("idToken", rel.data.idToken);
+				localStorage.setItem("expiresIn", new Date(new Date().getTime() + rel.data.expiresIn*1000));
+				this.$router.push("/loginhome");
+				this.$store.dispatch("autoLogout");
+			},(err) => {
+				console.log(err);
 			})
 		}
 	}
@@ -63,30 +55,31 @@ export default{
 
 
 <style scoped>
-.jumbo{
+.jumbo {
 	background: #100E17;	
 	width: 60%;
 	margin: 20px auto;	
 	box-shadow: 2px 4px 10px 3px black;
 }	
 
-.login{
+.login {
 	padding-top: 100px;
-
 	padding-bottom: 12px;
-
 }
-label{
+
+label {
 	color: white;
 	font-family: ubuntu;
 	font-size: 25px;
 }
-button{
-	background:#100E17;
 
+button {
+	background:#100E17;
 }
-button:hover{
+
+button:hover {
 	background:#100E17;
 	box-shadow: 2px 4px 10px 3px black;
 }
+
 </style>
